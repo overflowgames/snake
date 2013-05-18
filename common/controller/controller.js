46b79c5e2a8519ef7fb220c35fedafdcddab0821
Arthur <spam.00456@gmail.com>
2013-05-18 14:52:16 -0400

    Finition de la structure du controleur, manque plus que l'algo a faire

--- common/controller/controller.js
+++ common/controller/controller.js
@@ -1,10 +1,26 @@
-function Controller (){
+function Controller (update_rate){
     this.snakes = {};
-    this.addSnake = function (snake) {
-        this.snakes[snake.id] = snake;
+    this.addSnake = function (id, coords, direction) {
+        this.snakes[id].coords = coords;
+        this.snakes[id].direction = direction;
     };
+    
     this.killSnake = function (id) {
         delete this.snakes[id];
     };
     
+    this.changeDirection = function (id, direction) {
+        this.snakes[id].direction = direction;
+    };
+    
+    this.getSnake = function (id) {
+        return this.snakes[id];
+    };
+    
+    this.update = function () {     // This is where the magic happens
+        
+    };
+
+    setInterval(this.update, (1/update_rate)*1000); // Update the game regularly
+
 }
