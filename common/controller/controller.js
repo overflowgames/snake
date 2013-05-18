f3ab167fe773244eeada0c39956d509603392893
Arthur <spam.00456@gmail.com>
2013-05-18 03:40:25 -0400

    Debut d'implementation du controlleur

--- common/controller/controller.js
+++ common/controller/controller.js
@@ -1,3 +1,10 @@
 function Controller (){
+    this.snakes = {};
+    this.addSnake = function (snake) {
+        this.snakes[snake.id] = snake;
+    };
+    this.killSnake = function (id) {
+        delete this.snakes[id];
+    };
     
 }
