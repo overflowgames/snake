3765e249282502aa9fc4bca83ae90cd965eb3e15
Arthur <spam.00456@gmail.com>
2013-05-19 06:02:07 -0400

    ajout de callbacks pour rapeller quand un snake a été bouffé et ajout d'une fonction pour ajouter un bonnus

--- common/controller/controller.js
+++ common/controller/controller.js
@@ -1,5 +1,9 @@
-function Controller (update_rate){
+function Controller (options){
     this.snakes = {};
+    this.bonnus = [];
+    this.killed_snake_callback = options.killed_snake;
+    this.eaten_bonnus_callback = options.eaten_bonnus;
+    
     this.addSnake = function (id, coords, direction) {
         this.snakes[id].coords = coords;
         this.snakes[id].direction = direction;
@@ -17,10 +21,14 @@ function Controller (update_rate){
         return this.snakes[id];
     };
     
+    this.addBonnus = function (coords) {
+        this.bonnus.push(coords);
+    };
+    
     this.update = function () {     // This is where the magic happens
         
     };
 
-    setInterval(this.update, (1/update_rate)*1000); // Update the game regularly
+    setInterval(this.update, (1/options.update_rate)*1000); // Update the game regularly
 
 }
