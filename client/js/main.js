95b31411c7117e2b8d7d63b6a51c471850416dc5
Lucas <lucas.pluvinage@gmail.com>
2013-10-10 13:57:19 -0400

    Rendu trivial des bonus

--- client/js/main.js
+++ client/js/main.js
@@ -102,6 +102,15 @@ function update_canvas(snakes, bonus) {
         }
     }
     
+    // #Draw bonuses
+    for(var i in bonus) {
+        var cx = bonus.coords[0];
+        var cy = bonus.coords[1];
+        
+        context.fillStyle = "#ffaa00";
+        context.fillRect(cx*sq_w-position_x+offset_x, cy*sq_w-position_y+offset_y, sq_w, sq_w);
+    }
+    
     
     // #Draw the grid
     context.strokeStyle = "#ffffff";
